0692ebede98d2bd6a68caeabb615be4b429ebe78
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2024-08-03 18:12:02 +0700

    fixing bug tapi belom jadi

--- express.js
+++ express.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const PORT = 3000;
+const PORT = 5500;
 
-// Middleware untuk menangani permintaan ke folder CSS tanpa menyebutkan nama file ( masih ada bug sepertinya)
+// // Middleware untuk menangani permintaan ke folder CSS tanpa menyebutkan nama file ( masih ada bug sepertinya)
 app.use("/css", (req, res, next) => {
-  const cssPath = path.join(__dirname, "css", "bootstrap.min.css");
+  const cssPath = path.join(__dirname, "css", "bootstrap.css");
   fs.readFile(cssPath, (err, data) => {
     if (err) {
       res.status(404).send("CSS file not found");
@@ -17,12 +17,53 @@ app.use("/css", (req, res, next) => {
   });
 });
 
-// Set folder static untuk JavaScript dan file lain
+// ----------------- //
+
+// // Middleware untuk menggabungkan file CSS
+// app.use("/css/", (req, res) => {
+//   const cssFiles = [
+//     path.join(__dirname, "css", "style.css"),
+//     path.join(__dirname, "css", "bootstrap.css"),
+//     path.join(__dirname, "css", "bootstrap.min.css")
+//     path.join(__dirname, "css", "bootstrap.rtl.css")
+//     path.join(__dirname, "css", "bootstrap.rtl.min.css")
+//     path.join(__dirname, "css", "bootstrap-grid.css")
+//     path.join(__dirname, "css", "bootstrap-grid.min.css")
+//     path.join(__dirname, "css", "bootstrap-grid.rtl.css")
+//     path.join(__dirname, "css", "bootstrap-grid.rtl.min.css")
+//     path.join(__dirname, "css", "bootstrap-reboot.css")
+//     path.join(__dirname, "css", "bootstrap-reboot.min.css")
+//     path.join(__dirname, "css", "bootstrap-reboot.rtl.css")
+//     path.join(__dirname, "css", "bootstrap-reboot.rtl.min.css")
+//     path.join(__dirname, "css", "bootstrap-utilities.css")
+//     path.join(__dirname, "css", "bootstrap-utilities.min.css")
+//     path.join(__dirname, "css", "bootstrap-utilities.rtl.css")
+//     path.join(__dirname, "css", "bootstrap-utilities.rtl.min.css")
+//   ];
+
+ 
+//   Promise.all(cssFiles.map(file => fs.promises.readFile(file)))
+//     .then(data => {
+    
+//       const combinedCss = data.join("\n");
+
+     
+//       res.set("Content-Type", "text/css");
+//       res.send(combinedCss);
+//     })
+//     .catch(err => {
+    
+//       res.status(500).send("Error membaca CSS files");
+//     });
+// });
+
+// // Set folder static untuk JavaScript dan file lain
 app.use("/js", express.static(path.join(__dirname, "js")));
 
-// Routing untuk jalankan ke halaman utama
+// // Routing untuk jalankan ke halaman utama
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
